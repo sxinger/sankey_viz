601298eca747f6025f174dc425a6fcce843da89a
sxinger <sxingerl@gmail.com>
2018-08-09 17:24:49 -0500

    change data extension

--- R/save_agg_data.R
+++ R/save_agg_data.R
@@ -1,12 +1,13 @@
 ####read local data file and save to /data####
-
 dat_path<-"E:/Sepsis Treatment FS PM/sankey_dat.Rdata"
-dat<-load(dat_path)
+load(dat_path)
+save(sankey_dat,file="./data/sankey_dat.rda")
 
-save(dat,file="./data/sankey_dat.rda")
 
 #test
 rm(list=ls());gc()
-dat<-data("sankey_dat")
-head(dat)
+data()
+data("sankey_dat")
+head(sankey_dat)
+
 
